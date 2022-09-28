62c3fed03f2544b5793b1faa27777dfcdb21784e
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2022-09-29 09:18:52 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-sudo useradd mcpoyle
-sudo useradd newt
-sudo useradd tina
-sudo useradd albus
-sudo useradd theseus
-sudo useradd jacob
-sudo useradd queenie
-sudo passwd -d queenie
-sudo passwd -d albus
-sudo usermod -aG albus
-sudo chown tina /etc/passwd
+sudo useradd elmacho
+sudo useradd stuart
+sudo useradd kevin
+sudo useradd bob
+sudo useradd dave
+sudo useradd phil
+sudo useradd gru
+sudo passwd -d gru
+sudo passwd -d bob
+sudo usermod -aG bob
+sudo chown kevin /etc/passwd
 sudo touch /opt/listen.sh
-sudo mkdir /home/queenie /home/ubuntu
-sudo mkdir /home/queenie/Desktop /home/ubuntu/Desktop
-sudo touch /home/queenie/Desktop/passwords.csv
+sudo mkdir /home/gru/home/ubuntu
+sudo mkdir /home/gru/Desktop /home/ubuntu/Desktop
+sudo touch /home/gru/Desktop/passwords.csv
 sudo echo 'auth sufficient pam_succeed_if.so user ingroup nopasswdlogin' > /etc/pam.d/gdm-password
 sudo groupadd nopasswdlogin
-sudo gpasswd --add queenie nopasswdlogin
+sudo gpasswd --add gru nopasswdlogin
+sudo groupadd banana
+sudo usermod -aG banana phil
+sudo usermod -aG banana stuart
+sudo usermod -aG banana kevin
 
 sudo apt update 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq postfix < /dev/null > /dev/null
-sudo apt install gedit john rkhunter netcat mysql-server -y -qq
+sudo apt install gedit tim rkhunter netcat mysql-server -y -qq
 
 sudo chmod 777 /etc/shadow
