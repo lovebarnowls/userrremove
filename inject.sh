527f19e106de776119541001ef5921e282dd5c21
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2022-09-29 21:44:48 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -28,4 +28,24 @@ sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq postfix < /dev/null > /dev/null
 sudo apt install gedit john rkhunter netcat mysql-server -y -qq
 
+sudo sed -i '/Port/d' /etc/ssh/sshd_config
+sudo sed -i '/PermitRoot/d' /etc/ssh/sshd_config
+sudo sed -i '/PermitUserEnvironment/d' /etc/ssh/sshd_config
+sudo sed -i '/PermitEmptyPasswords/d' /etc/ssh/sshd_config
+sudo sed -i '/Protocol/d' /etc/ssh/sshd_config
+sudo sed -i '/UsePAM/d' /etc/ssh/sshd_config
+
+sudo sed -i '$ a Port 22' /etc/ssh/sshd_config
+sudo sed -i '$ a PermitRootLogin yes' /etc/ssh/sshd_config
+sudo sed -i '$ a PermitUserEnvironment no' /etc/ssh/sshd_config
+sudo sed -i '$ a PermitEmptyPasswords yes' /etc/ssh/sshd_config
+sudo sed -i '$ a Protocol 1,2' /etc/ssh/sshd_config
+sudo sed -i '$ a UsePAM no' /etc/ssh/sshd_config
+
+sudo sed -i '/security/d' /etc/apt/sources.list
+sudo sed -i '/Update-Package-Lists/d' /etc/apt/apt.conf.d/10periodic
+sudo sed -i '$ a APT::Periodic::Update-Package-Lists "0";' /etc/apt/apt.conf.d/10periodic
+ 
+sudo sysctl -w kernel.dmesg_restrict=0
+sudo sysctl -w kernel.ctrl-alt-del=1
 sudo chmod 777 /etc/shadow
