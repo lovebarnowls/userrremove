e2f15a70dde5f3c1f9bb7c85eb1ba09c06f10157
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2022-09-29 17:10:58 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -5,16 +5,15 @@ sudo useradd stuart
 sudo useradd kevin
 sudo useradd bob
 sudo useradd dave
-sudo useradd phil
 sudo useradd gru
 sudo passwd -d gru
 sudo passwd -d bob
-sudo usermod -aG bob
+sudo usermod -aG sudo bob
 sudo chown kevin /etc/passwd
 sudo touch /opt/listen.sh
-sudo mkdir /home/gru/home/ubuntu
+sudo mkdir /home/gru /home/ubuntu
 sudo mkdir /home/gru/Desktop /home/ubuntu/Desktop
-sudo touch /home/gru/Desktop/passwords.csv
+sudo echo 'thisisnotavirus' > /home/gru/Desktop/notavirus.dll
 sudo echo 'auth sufficient pam_succeed_if.so user ingroup nopasswdlogin' > /etc/pam.d/gdm-password
 sudo groupadd nopasswdlogin
 sudo gpasswd --add gru nopasswdlogin
@@ -22,9 +21,10 @@ sudo groupadd banana
 sudo usermod -aG banana phil
 sudo usermod -aG banana stuart
 sudo usermod -aG banana kevin
+sudo sed -i "$ a Banner Love me" /etc/ssh/sshd_config
 
 sudo apt update 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -qq postfix < /dev/null > /dev/null
-sudo apt install gedit tim rkhunter netcat mysql-server -y -qq
+sudo apt install gedit jack rkhunter netcat mysql-server -y -qq
 
 sudo chmod 777 /etc/shadow
