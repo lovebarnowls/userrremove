d23577199d54ed20ec387413c2869610b2ea9486
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2022-09-29 18:18:03 +1000

    Update wallpaper.sh

--- wallpaper.sh
+++ wallpaper.sh
@@ -6,8 +6,8 @@ wget https://www.wallpapers13.com/wp-content/uploads/2017/12/Walt-Disney-The-Sto
 wget https://www.wallpapers13.com/wp-content/uploads/2017/11/Clumsy-Smurfs-Smurfette-Hefty-Smurfs-and-Brainy-Smurfs-The-Movie-The-Lost-Village-Screenshot-1920x1080-1920x1080.jpg
 wget https://www.wallpapers13.com/wp-content/uploads/2017/12/Seven-Dwarfs-caracters-Doc-Grumpy-Sneezy-Sleepy-Happy-Bashful-and-Dopey-HD-Wallpaper-for-Desktop1920x1200-1920x1080.jpg
 
-dconf write /org/mate/desktop/background/picture-filename "'/home/ubuntu/Pictures/avengers-infinity-war-4k-hm-1920x1080.jpg'"
-# dconf write /org/mate/desktop/background/picture-filename "'/home/ubuntu/Pictures/minion-5.jpg'"
+#dconf write /org/mate/desktop/background/picture-filename "'/home/ubuntu/Pictures/avengers-infinity-war-4k-hm-1920x1080.jpg'"
+ dconf write /org/mate/desktop/background/picture-filename "'/home/ubuntu/Pictures/minion-5.jpg'"
 # dconf write /org/mate/desktop/background/picture-filename "'/home/ubuntu/Pictures/Star-Wars-episode-iv-caracters-Harrison-Ford-Darth-Vader-Carrie-Fisher-Luke-Skywalker-Chewbacca-Wallpaper-HD-2560x1440-1920x1080.jpg'"
 # dconf write /org/mate/desktop/background/picture-filename "'/home/ubuntu/Pictures/Walt-Disney-The-Story-of-Aladdin-and-Princess-Jasmine-Gin-Sultan-and-Abu-Monkey-HD-Wallpaper-1920x1200-840x525.jpg'"
 # dconf write /org/mate/desktop/background/picture-filename "'/home/ubuntu/Pictures/Clumsy-Smurfs-Smurfette-Hefty-Smurfs-and-Brainy-Smurfs-The-Movie-The-Lost-Village-Screenshot-1920x1080-1920x1080.jpg'"
