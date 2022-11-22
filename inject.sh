716cbccecb6c19ead1b7f633f7fe1e0093239b07
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2022-11-22 13:04:21 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-sudo useradd elmacho
-sudo useradd stuart
-sudo useradd kevin
-sudo useradd bob
-sudo useradd vector
-sudo useradd gru
-sudo useradd phil
-sudo passwd -d gru
-sudo passwd -d bob
-sudo usermod -aG sudo bob
-sudo chown kevin /etc/passwd
+sudo useradd hatshepsut
+sudo useradd theophilus
+sudo useradd lysander
+sudo useradd elliott
+sudo useradd amenhotepiii
+sudo useradd kyrie
+sudo useradd ariadne
+sudo passwd -d kyrie
+sudo passwd -d elliott
+sudo usermod -aG sudo elliott
+sudo chown lysander /etc/passwd
 sudo touch /opt/listen.sh
-sudo mkdir /home/gru /home/ubuntu
-sudo mkdir /home/gru/Desktop /home/ubuntu/Desktop
-sudo echo 'thisisnotavirus' > /home/gru/Desktop/notavirus.dll
-sudo echo 'auth sufficient pam_succeed_if.so user ingroup nopasswdlogin' > /etc/pam.d/gdm-password
+sudo mkdir /home/kyrie /home/ubuntu
+sudo mkdir /home/kyrie/Desktop /home/ubuntu/Desktop
 sudo groupadd nopasswdlogin
-sudo gpasswd --add gru nopasswdlogin
-sudo groupadd apple
-sudo usermod -aG apple phil
-sudo usermod -aG apple stuart
-sudo usermod -aG apple kevin
+sudo gpasswd --add kyrie nopasswdlogin
