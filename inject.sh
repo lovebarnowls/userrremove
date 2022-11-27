d7cc416693615a398f88897f5ecab35ff29efc3d
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2022-11-27 20:33:04 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 sudo useradd hatshepsut
-sudo useradd theophilus
-sudo useradd lysander
+sudo useradd farhan
+sudo useradd abdulrahman
 sudo useradd elliott
 sudo useradd amenhotepiii
 sudo useradd kyrie
 sudo useradd ariadne
-sudo useradd nicholas
+sudo useradd laaibah
 sudo passwd -d kyrie
 sudo passwd -d elliott
 sudo usermod -aG sudo elliott
-sudo chown lysander /etc/passwd
-sudo touch /opt/listen.sh
-sudo mkdir /home/kyrie /home/ubuntu
-sudo mkdir /home/kyrie/Desktop /home/ubuntu/Desktop
-sudo groupadd nopasswdlogin
-sudo gpasswd --add kyrie nopasswdlogin
+sudo chown abdulrahman /etc/passwd
+
