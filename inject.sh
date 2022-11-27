c997f48b7b4930890c9902b4e2a84fc2707cc100
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2022-11-27 20:41:24 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -1,15 +1,52 @@
 #!/bin/bash
 
-sudo useradd hatshepsut
 sudo useradd farhan
 sudo useradd abdulrahman
-sudo useradd elliott
-sudo useradd amenhotepiii
-sudo useradd kyrie
-sudo useradd ariadne
 sudo useradd laaibah
-sudo passwd -d kyrie
-sudo passwd -d elliott
-sudo usermod -aG sudo elliott
+sudo useradd adham
+sudo useradd adib
+sudo useradd adil
+sudo useradd adiy
+sudo useradd adnan
+sudo useradd adyan
+sudo useradd aftab
+sudo useradd aftaab
+sudo useradd dawid
+sudo useradd dawiah
+sudo useradd babar
+sudo useradd baqar
+sudo useradd basel
+sudo useradd basem
+sudo useradd zafar
+sudo useradd zafir
+sudo useradd zafrul
+sudo useradd zahir
+sudo useradd zakar
+sudo useradd sadat
+sudo useradd saadat
+sudo useradd saad
+sudo useradd sabbir
+sudo useradd sabir
+sudo useradd sabur
+sudo useradd kaleem
+sudo useradd kaleema
+sudo useradd kamal
+sudo useradd kamil
+sudo useradd karim
+sudo useradd kasam
+sudo useradd kasim
+sudo useradd kashan
+sudo useradd kaysan
+sudo useradd ghazanfar
+sudo useradd ghazawan
+sudo useradd nadir
+sudo useradd naadir
+sudo useradd nabeel
+sudo useradd nadim
+sudo useradd nadr
+sudo useradd naeem
+sudo passwd -d sadat
+sudo passwd -d naim
+sudo usermod -aG sudo naim
 sudo chown abdulrahman /etc/passwd
 
